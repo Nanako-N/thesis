c927be3684157589795ef8d2b4c2aff905fa268c
Nanako-N <g16929nn@gm.tsuda.ac.jp>
2019-11-22 16:16:39 +0900

    ボタン追加

--- blog/models.py
+++ blog/models.py
@@ -38,7 +38,7 @@ def FileRead(t):
 
 NumberOfPeople = ((2,2),(3,3),(4,4),(5,5),)
 DESTINATION = ((True,'あり'),(False,'なし'),)
-Exit = FileRead("exit.txt")
+Exit = FileRead("nanako.pythonanywhere.com/exit.txt")
 Landmark = FileRead("landmark.txt")
 
 class Group(models.Model):
